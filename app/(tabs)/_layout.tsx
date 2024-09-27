e2bbdb9a05565a265ec25a382b7341c17c95774b
Saul Lee <solleedata@gmail.com>
2024-09-27 16:56:51 +0900

    refactor: Remove unused files

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,15 +3,11 @@ import React from 'react';
 
 import { TabBarIcon } from '@/components/navigation/TabBarIcon';
 import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
         tabBarStyle: {
           // display: route.name === 'example' ? 'none' : 'flex',
