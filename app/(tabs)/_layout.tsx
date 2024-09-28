fd7ea778682ea0ac6511ed16c468ed35ee8901c1
Saul Lee <solleedata@gmail.com>
2024-09-28 10:39:02 +0900

    refactor: Remove unused components and constants

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,9 +1,6 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
 
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-
 export default function TabLayout() {
   return (
     <Tabs
@@ -17,10 +14,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
-          ),
+          title: 'Home'
         }}
       />
     </Tabs>
